4991c9ba15ef618b98510ffda2a111fc6269befb
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-10-14 13:47:58 +0200

    Created and detailed the Brand class.

---
+++ KFKWS3_HFT_2021221.Models/Brand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Models
+{
+
+    [Table("brands")]
+    public class Brand
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+        
+        [NotMapped]
+        public virtual ICollection<Car> Cars { get; set; }
+
+        public Brand()
+        {
+            Cars = new HashSet<Car>();
+        }
+    }
+}
