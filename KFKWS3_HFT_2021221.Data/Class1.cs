ab2771e5575cb25f77d494c7093ecc1e0b0b3076
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-10-14 13:47:34 +0200

    Changed the database properties to content and copy always.

--- KFKWS3_HFT_2021221.Data/Class1.cs
+++ KFKWS3_HFT_2021221.Data/Class1.cs
@@ -2,7 +2,10 @@
 
 namespace KFKWS3_HFT_2021221.Data
 {
+
+    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\KFKWS3_HFT_2021221.Database.mdf;Integrated Security=True;MultipleActiveResultSets=True;
     public class Class1
     {
+
     }
 }
