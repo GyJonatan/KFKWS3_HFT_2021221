659cc7853843bfd63022b8894730ebbd933c3a49
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-10-19 18:33:21 +0200

    Created the Brand repository alongside with the corresponding CRUD interface.

---
+++ KFKWS3_HFT_2021221.Repository/BrandRepository.cs
@@ -0,0 +1,59 @@
+﻿using KFKWS3_HFT_2021221.Data;
+using KFKWS3_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Repository
+{
+    class BrandRepository : IBrandRepository
+    {
+        KFKWS3DbContext context;
+
+        public BrandRepository(KFKWS3DbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Brand brand)
+        {
+            context.Brands.Add(brand);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {         
+            Brand brand = ReadOne(id);
+
+            context.Brands.Remove(brand);
+            context.SaveChanges();
+        }
+
+        public Brand ReadOne(int id)
+        {
+            return context
+                .Brands
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<Brand> ReadAll()
+        {
+            return context.Brands;
+        }
+
+        public void Update(Brand brand)
+        {
+            Brand old = ReadOne(brand.Id);
+
+            if (old == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            old.Id = brand.Id;
+            old.Name = brand.Name;
+        }
+    }
+}
---
+++ KFKWS3_HFT_2021221.Repository/IBrandRepository.cs
@@ -0,0 +1,18 @@
+﻿using KFKWS3_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Repository
+{
+    interface IBrandRepository
+    {
+        void Create(Brand car);
+        Brand ReadOne(int id);
+        IQueryable<Brand> ReadAll();
+        void Update(Brand car);
+        void Delete(int id);
+    }
+}
