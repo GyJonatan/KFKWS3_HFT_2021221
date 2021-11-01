8e9d7782920187bded5128cc2a8e8f3c35cce0ba
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-11-01 11:57:29 +0100

    LeasingLogic class and interface

---
+++ KFKWS3_HFT_2021221.Logic/ILeasingLogic.cs
@@ -0,0 +1,17 @@
+﻿using KFKWS3_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Logic
+{
+    interface ILeasingLogic : ILogic<Leasing>
+    {
+        void ChangeCompanyName(int id, string newName);
+
+        string MostPayingCompany();
+
+    }
+}
---
+++ KFKWS3_HFT_2021221.Logic/LeasingLogic.cs
@@ -0,0 +1,26 @@
+﻿using KFKWS3_HFT_2021221.Models;
+using KFKWS3_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Logic
+{
+    public class LeasingLogic : Logic<Leasing>, ILeasingLogic
+    {
+        public LeasingLogic(ILeasingRepository repository) : base(repository) { }        
+        public void ChangeCompanyName(int id, string newName)
+        {
+            (repository as ILeasingRepository).ChangeCompanyName(id, newName);
+        }
+
+        public string MostPayingCompany()
+        {
+            //This is something i have to come up with later on.
+            //It'll show the name of the company that pays the most for it's leased cars.
+            return null; //for the meantime the result is null.
+        }
+    }
+}
