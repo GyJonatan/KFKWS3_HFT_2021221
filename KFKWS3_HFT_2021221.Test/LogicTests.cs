7b1132df025e27d817a7f00e4daba38015c70250
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-11-01 15:51:21 +0100

    Wrote 2 CRUD and 2 Non-CRUD test within the corresponding .cs file using Moq

---
+++ KFKWS3_HFT_2021221.Test/LogicTests.cs
@@ -0,0 +1,61 @@
+﻿using KFKWS3_HFT_2021221.Models;
+using KFKWS3_HFT_2021221.Repository;
+using KFKWS3_HFT_2021221.Logic;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KFKWS3_HFT_2021221.Test
+{
+    //NuGet: Nunit,Nunit3TestAdapter, Moq, Microsoft.NET.Test.Sdk
+    //5x Non-crud
+    //3x Create
+    //2x Something
+
+    [TestFixture]
+    public class LogicTests
+    {
+       [Test] 
+       public void TestGetByBrand()
+        {
+            Mock<ICarRepository> mockedRepository = new Mock<ICarRepository>(MockBehavior.Loose);
+
+            List<Car> cars = new List<Car>
+            {
+                new Car() { Model = "i440", BrandId = 1},
+                new Car() { Model = "i440 Cabrio", BrandId = 1 },
+                new Car() { Model = "Fiesta", BrandId = 2},
+                new Car() { Model = "Arteon", BrandId = 3 }
+            };
+
+            List<Car> expectedCars = new List<Car>() { cars[0], cars[1] };
+            
+            mockedRepository.Setup(x => x.ReadAll()).Returns(cars.AsQueryable());
+            CarLogic logic = new CarLogic(mockedRepository.Object);
+
+            var result = logic.GetCarsByBrand(1);
+
+            Assert.That(result.Count, Is.EqualTo(expectedCars.Count));
+            Assert.That(result, Is.EquivalentTo(expectedCars));
+
+            mockedRepository.Verify(x => x.ReadAll(), Times.Once);
+            mockedRepository.Verify(x => x.ReadOne(It.IsAny<int>()), Times.Never);
+        }
+
+        [Test]
+        public void TestAddBrand()
+        {
+            Mock<IBrandRepository> brandRepository = new Mock<IBrandRepository>();
+            brandRepository.Setup(x => x.Add(It.IsAny<string>())).Returns(42);
+            BrandLogic logic = new BrandLogic(brandRepository.Object);
+
+            int idNumber = logic.AddBrand("BMW");
+
+            Assert.That(idNumber, Is.EqualTo(42));
+            brandRepository.Verify(x => x.Add("BMW"), Times.Once);
+        }
+        
+    }
+}
---
+++ KFKWS3_HFT_2021221.Test/QueryTests.cs
@@ -0,0 +1,90 @@
+﻿using KFKWS3_HFT_2021221.Logic;
+using KFKWS3_HFT_2021221.Models;
+using KFKWS3_HFT_2021221.Repository;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Test
+{
+    [TestFixture]
+    public class QueryTests
+    {
+        Mock<ICarRepository> carRepository;
+        Mock<IBrandRepository> brandRepository;
+        List<AveragesResult> expectedAverages;
+
+        private CarAndBrandQuery CreateCarAndBrandQueryLogicWithMocks()
+        {
+            carRepository = new Mock<ICarRepository>();
+            brandRepository = new Mock<IBrandRepository>();
+
+            Brand bmw = new Brand() { Id = 1, Name = "BMW" };
+            Brand ford = new Brand() { Id = 2, Name = "Ford" };
+            List<Brand> brands = new List<Brand>() { bmw, ford };
+            List<Car> cars = new List<Car>()
+            {
+                new Car() { Brand = bmw, BrandId = bmw.Id, BasePrice = 16000},
+                new Car() { Brand = bmw, BrandId = bmw.Id, BasePrice = 15000},
+                new Car() { Brand = ford, BrandId = ford.Id, BasePrice = 12000}
+            };
+            expectedAverages = new List<AveragesResult>()
+            {
+                new AveragesResult() {BrandName = "BMW", AveragePrice = 15500},
+                new AveragesResult() {BrandName = "Ford", AveragePrice = 12000}
+            };
+
+            carRepository.Setup(x => x.ReadAll()).Returns(cars.AsQueryable());
+            brandRepository.Setup(x => x.ReadAll()).Returns(brands.AsQueryable());
+            return new CarAndBrandQuery(carRepository.Object, brandRepository.Object);
+        }
+
+        private CarLogic CreateCarLogicWithMocks()
+        {
+            carRepository = new Mock<ICarRepository>();
+
+            Brand bmw = new Brand() { Id = 1, Name = "BMW" };
+            Brand ford = new Brand() { Id = 2, Name = "Ford" };
+
+            List<Car> cars = new List<Car>()
+            {
+                new Car() { Brand = bmw, BrandId = bmw.Id, BasePrice = 16000},
+                new Car() { Brand = bmw, BrandId = bmw.Id, BasePrice = 15000},
+                new Car() { Brand = ford, BrandId = ford.Id, BasePrice = 12000}
+            };
+            expectedAverages = new List<AveragesResult>()
+            {
+                new AveragesResult() {BrandName = "BMW", AveragePrice = 15500},
+                new AveragesResult() {BrandName = "Ford", AveragePrice = 12000}
+            };
+
+            carRepository.Setup(x => x.ReadAll()).Returns(cars.AsQueryable());
+            return new CarLogic(carRepository.Object);
+        }
+
+        [Test]
+        public void TestGetAveragesJoin()
+        {
+            var logic = CreateCarAndBrandQueryLogicWithMocks();
+            var actualAverages = logic.GetBrandAveragesJoin();
+
+            Assert.That(actualAverages, Is.EquivalentTo(expectedAverages));
+            carRepository.Verify(car => car.ReadAll(), Times.Once);
+            brandRepository.Verify(brand => brand.ReadAll(), Times.Once);
+        }
+
+        [Test]
+        public void TestGetAverages()
+        {
+            var logic = CreateCarLogicWithMocks();
+            var actualAverages = logic.GetBrandAverages();
+
+            Assert.That(actualAverages, Is.EquivalentTo(expectedAverages));
+            carRepository.Verify(car => car.ReadAll(), Times.Once);
+        }
+    }
+}
