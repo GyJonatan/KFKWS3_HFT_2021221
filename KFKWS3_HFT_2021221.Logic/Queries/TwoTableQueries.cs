703a19e07a3f3a45a87502b01172d4d98add510d
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-11-17 00:24:29 +0100

    Created two classes for queries depending on the amount of tables used and made 3 result classes for the 3 query written in the TwoTableQueries class. They were put into their respective folder

---
+++ KFKWS3_HFT_2021221.Logic/Queries/TwoTableQueries.cs
@@ -0,0 +1,73 @@
+﻿using KFKWS3_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Logic
+{
+    public class TwoTableQueries
+    {
+        ICarRepository carRepository;
+        IBrandRepository brandRepository;
+        ILeasingRepository leasingRepository;
+        public TwoTableQueries
+        (ICarRepository carRepository, IBrandRepository brandRepository, ILeasingRepository leasingRepository)
+        {
+            this.carRepository = carRepository;
+            this.brandRepository = brandRepository;
+            this.leasingRepository = leasingRepository;
+        }
+        public IEnumerable<AveragesResult> GetBrandAverages()
+        {
+            return (from car in carRepository.ReadAll()
+                    join brand in brandRepository.ReadAll() on car.BrandId equals brand.Id
+                    let item = new { BrandName = brand.Name, Price = car.BasePrice }
+                    group item by item.BrandName into grp
+                    select new AveragesResult()
+                    {
+                        BrandName = grp.Key,
+                        AveragePrice = grp.Average(item => item.Price) ?? 0
+                    }).ToList();
+        }
+        //unit test
+        public IEnumerable<MostCarsResult> GetBrandsByPopularity()
+        {
+            return (from brand in brandRepository.ReadAll()
+                    join leasing in leasingRepository.ReadAll() on brand.LeasingId equals leasing.Id
+                    let item = new { BrandName = brand.Name, NumberOfCars = brand.Cars.Count }
+                    orderby item.NumberOfCars descending
+                    select new MostCarsResult()
+                    {
+                        BrandName = item.BrandName,
+                        LeasingName = leasing.Name,
+                        NumberOfCars = item.NumberOfCars
+                    }).ToList();
+        }
+        //unit test
+        public IEnumerable<BudgetResult> GetDays()
+        {
+            return (brandRepository.ReadAll()
+                     .Join(leasingRepository.ReadAll(),
+                     brand => brand.LeasingId,
+                     leasing => leasing.Id,
+                     (brand, leasing) => new
+                     {
+                         leasingName = leasing.Name,
+                         budget = leasing.Budget,
+                         sumPrice = brand.Cars.Sum(x => x.BasePrice) ?? 0
+                     })
+                     .OrderBy(x => x.budget / x.sumPrice)
+                     .Select(x => new BudgetResult()
+                     {
+                         leasingName = x.leasingName,
+                         amountOfDays = x.budget / x.sumPrice
+                     })).ToList();
+        }
+
+        //come up with something
+        
+
+    }
+}
---
+++ KFKWS3_HFT_2021221.Logic/Results/MostCarsResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Logic
+{
+    public class MostCarsResult
+    {
+        public string BrandName { get; set; }
+        public string LeasingName { get; set; }
+        public int NumberOfCars { get; set; }
+        public override string ToString()
+        {
+            return $"Leasing: {LeasingName}\t Brand: {BrandName} ({NumberOfCars})";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is MostCarsResult)
+            {
+                MostCarsResult other = obj as MostCarsResult;
+                return this.BrandName == other.BrandName 
+                    && this.LeasingName == other.LeasingName
+                    && this.NumberOfCars == other.NumberOfCars;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return BrandName.GetHashCode() + LeasingName.GetHashCode() + NumberOfCars;
+        }
+    }
+}
