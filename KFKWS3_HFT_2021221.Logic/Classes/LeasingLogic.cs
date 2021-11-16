871b790c2d3669232d11e6af1358e1e0e3732a2c
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-11-17 00:22:25 +0100

    redesigned the logic classes and interfaces into folders

---
+++ KFKWS3_HFT_2021221.Logic/Classes/LeasingLogic.cs
@@ -0,0 +1,40 @@
+﻿using KFKWS3_HFT_2021221.Models;
+using KFKWS3_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Logic
+{
+    public class LeasingLogic : Logic<Leasing>, ILeasingLogic
+    {
+        public LeasingLogic(ILeasingRepository repository) : base(repository) { }        
+        public void ChangeCompanyName(int id, string newName)
+        {
+            (repository as ILeasingRepository).ChangeCompanyName(id, newName);
+        }
+
+        public void Update(Leasing leasing)
+        {
+            (repository as ILeasingRepository).Update(leasing);
+        }
+
+
+
+        //useful for later reference
+
+        //public static void AddToCompanyList<T>(this Leasing leasing, Company company)
+        //{
+        //    if (!leasing.Companies.Contains(company))
+        //    {
+        //        leasing.Companies.Add(company);
+        //    }
+        //    else
+        //    {
+        //        throw new InvalidOperationException("Company is already on the list");
+        //    }
+        //}
+    }
+}
---
+++ KFKWS3_HFT_2021221.Logic/Classes/Logic.cs
@@ -0,0 +1,26 @@
+﻿using KFKWS3_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Logic
+{
+    public abstract class Logic<T> : ILogic<T> where T : class
+    {
+        protected IRepository<T> repository;
+        public Logic(IRepository<T> repository)
+        {
+            this.repository = repository;
+        }
+        public IList<T> ReadAll()
+        {
+            return repository.ReadAll().ToList();
+        }
+        public T ReadOne(int id)
+        {
+            return repository.ReadOne(id);
+        }
+    }
+}
