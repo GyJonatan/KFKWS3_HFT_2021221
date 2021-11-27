d1d93a97262c9c94fd44c7d4a9d7e696a105226c
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-11-27 23:27:15 +0100

    Created and connected the client to the endpoint

--- KFKWS3_HFT_2021221.Client/Program.cs
+++ KFKWS3_HFT_2021221.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleTools;
+using KFKWS3_HFT_2021221.Models;
 using System;
 
 namespace KFKWS3_HFT_2021221.Client
@@ -11,6 +12,22 @@ namespace KFKWS3_HFT_2021221.Client
             //    .Add("Using Logic", () => Dosomething(logic))
             //    .Add("Close", ConsoleMenu.Close);
             //menu.Show();
+
+            System.Threading.Thread.Sleep(8000);
+
+            RestService rest = new RestService("http://localhost:63437");
+
+            rest.Post<Brand>(new Brand()
+            {
+                Name = "Peugeot"
+            }, "brand");
+
+            var cars = rest.Get<Car>("car");
+            var brands = rest.Get<Brand>("brand");
+            var leasings = rest.Get<Leasing>("leasing");
+
+
+            //var result1 = rest.Get<Car>("/car");
         }
     }
 }
