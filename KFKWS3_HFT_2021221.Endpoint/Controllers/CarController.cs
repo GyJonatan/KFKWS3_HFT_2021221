ba98510c06d7d443cf4ff0c1da9f7bc279591635
GyJonatan <gyurkovicsjonatan@hotmail.com>
2021-11-27 13:40:20 +0100

    Created Carcontroller class

---
+++ KFKWS3_HFT_2021221.Endpoint/Controllers/CarController.cs
@@ -0,0 +1,27 @@
+﻿using KFKWS3_HFT_2021221.Logic;
+using KFKWS3_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KFKWS3_HFT_2021221.Endpoint.Controllers
+{
+    public class CarController : Controller<Car>
+    {
+        public CarController(ICarLogic logic) : base(logic) { }
+
+        [HttpPut]
+        public override void Edit([FromBody] Car item)
+        {
+            (logic as ICarLogic).Update(item);
+        }
+
+        [HttpGet("changeprice/{id,price}")]
+        public void ChangePrice([FromRoute] int id, [FromRoute] int price)
+        {
+            (logic as ICarLogic).ChangePrice(id, price);
+        }
+    }
+}
